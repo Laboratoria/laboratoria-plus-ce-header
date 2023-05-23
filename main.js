2ff8d423ffb1bb143691000ef8928ad5c78a321f
Fares A. Akel C <f.antonio.akel@gmail.com>
2023-05-23 11:37:34 -0500

    refactor(main): Changes unicode to svg arrow

--- main.js
+++ main.js
@@ -36,7 +36,12 @@ class LaboratoriaHeader extends HTMLElement {
     const dropdown = document.createElement("div");
     dropdown.className = "dropdown";
     dropdown.innerHTML = `
-    <button class="dropbtn">Programas &#8964</button>
+    <button class="dropbtn">
+      Programas
+      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-chevron-down" viewBox="0 -1 14 14">
+        <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+      </svg>
+    </button>
     `;
 
     const programs = document.createElement("div");
